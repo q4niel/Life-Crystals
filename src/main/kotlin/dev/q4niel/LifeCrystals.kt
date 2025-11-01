5844db5956dab176a256bf782d63982afff5e6f1
q4niel <github.obscure938@passmail.net>
2025-11-01 14:18:45 +0100

    add player max health loss on death

--- src/main/kotlin/dev/q4niel/LifeCrystals.kt
+++ src/main/kotlin/dev/q4niel/LifeCrystals.kt
@@ -2,7 +2,14 @@ package dev.q4niel
 
 import dev.q4niel.item.ModItems
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents
+import net.fabricmc.fabric.api.networking.v1.PacketSender
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents
+import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.damage.DamageSource
+import net.minecraft.server.MinecraftServer
+import net.minecraft.server.network.ServerPlayNetworkHandler
+import net.minecraft.server.network.ServerPlayerEntity
 import org.slf4j.LoggerFactory
 
 object LifeCrystals : ModInitializer {
@@ -14,8 +21,23 @@ object LifeCrystals : ModInitializer {
     override fun onInitialize() {
         ModItems.init();
 
-        ServerPlayConnectionEvents.JOIN.register { handler, sender, server ->
+        ServerPlayConnectionEvents.JOIN.register {
+            handler: ServerPlayNetworkHandler,
+            sender: PacketSender,
+            server: MinecraftServer
+            ->
+
             PlayerPersistentData.initPlayerData(handler.player.uuid, handler.player);
+        }
+
+        ServerLivingEntityEvents.AFTER_DEATH.register {
+            entity: LivingEntity,
+            dmgSrc: DamageSource
+            ->
+
+            if (entity is ServerPlayerEntity) {
+                PlayerPersistentData.decrementMaxHealth(entity.uuid);
+            }
         }
     }
 }
